c6970979d43d962bb3ac5ebdb8d293e6c01895d4
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-06 00:15:52 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello Docker</h1>");
 });
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
